48687bad67429e7090fe20d454add80c457fe42c
Filip Figiel <filip.figiel@dcs.pl>
2015-11-07 16:21:16 +0000

    :clock1: 5m | Add a pipe in the commit msg suggestion

--- hooks/commit-msg.bash
+++ hooks/commit-msg.bash
@@ -2,7 +2,7 @@
 
 VALID="$(cat ${1} | grep ':clock' | wc -l)"
 if [[ $VALID -eq 0 ]]; then
-    echo "You have to include time in commit message, eg. \":clock1: 5m rest_of_message\" !"
+    echo "You have to include time in commit message, eg. \":clock1: 5m | rest_of_message\" !"
     echo "Aborting..."
     exit 1
 fi
