4fa5200c42dce919307d3b3b3bce234add0fb367
Piotr Pęczek <ppeczek@gmail.com>
2015-11-07 15:27:48 +0100

    :clock1: 5m | entities

---
+++ time_tracker/core/entities.py
@@ -0,0 +1,21 @@
+class Issue:
+    def __init__(self, number, name, msg):
+        self.number = number
+        self.name = name
+        self.msg = msg
+
+
+class Commit:
+    def __init__(self, author, msg, issue, time, src):
+        self.author = author
+        self.msg = msg
+        self.issue = issue
+        self.time = time
+        self.src = src
+
+
+class Commiter:
+    def __init__(self, name, email, user=None):
+        self.name = name
+        self.email = email
+        self.user = user
