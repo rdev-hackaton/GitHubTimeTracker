af29927472a9a5bf89acffd3a4b892f84b463575
Valian <jakub.skalecki@gmail.com>
2015-11-07 15:47:22 +0100

    :clock1: 2m fixed hook error description

--- hooks/commit-msg.bash
+++ hooks/commit-msg.bash
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-cat ${1} | grep clock | wc -l
-
+VALID="$(cat ${1} | grep ':clock' | wc -l)"
+if [[ $VALID -eq 0 ]]; then
+    echo "You have to include time in commit message, eg. \":clock1: 5m rest_of_message\" !"
+    echo "Aborting..."
+    exit 1
+fi
 exit 0
+
