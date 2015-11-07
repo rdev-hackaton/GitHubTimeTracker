1bb989734d67ba01e915f70574168a027f03f0d9
Lukasz Kozuchowski <Lukasz.Kozuchowski@gmail.com>
2015-11-07 21:28:11 +0100

    :clock1: 2m | flake8 fix

--- time_tracker/core/usecases.py
+++ time_tracker/core/usecases.py
@@ -9,7 +9,7 @@ def get_entries_list(data_source, repo_name, committer, issue, milestone):
         return {
             'result': Result.REPO_NOT_FOUND
         }
-    commiter = repo.get_by_commiter(author)
+    commiter = repo.get_by_commiter(committer)
     if not commiter:
         return {
             'result': Result.COMMITER_NOT_FOUND
@@ -21,7 +21,7 @@ def get_entries_list(data_source, repo_name, committer, issue, milestone):
     }
 
 
-def get_total_stats(data_source, repo_name, author, issue, milestone):
+def get_total_stats(data_source, repo_name, committer, issue, milestone):
     repo = data_source.get_repo(repo_name)
     if not repo:
         return {
