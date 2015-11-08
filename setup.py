47f156a7580b891852610d40bea6252a9ebbeba4
Marek Bednarski <marekb@marekb.waw.pl>
2015-11-08 01:43:25 +0100

    :clock1: 5m | Add CLI script to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 name = 'time_tracker'
 install_requires = [
     'click==5.1',
     'PyGithub==1.26.0',
 ]
+console_scripts = [
+    'ghttracker = time_tracker.frontends.cli.tracker:print_time_tracking_info',
+]
 
 if __name__ == '__main__':
     setup(
         name=name,
         install_requires=install_requires,
+        packages=find_packages(exclude=['hooks', 'tests']),
+        entry_points={
+            'console_scripts': console_scripts
+        }
     )
