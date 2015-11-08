bbea3fc65321a1e4b4f0e5ef5bb6b66408f7e0f5
Filip Figiel <filip.figiel@dcs.pl>
2015-11-08 02:58:08 +0000

    :clock1: 2m | Execute the hook only as __main__

--- hooks/commit-msg.py
+++ hooks/commit-msg.py
@@ -4,25 +4,27 @@ from random import choice
 
 from time_tracker.utils import parse_time_entry
 
-with open(sys.argv[1]) as f:
-    content = f.read()
+if __name__ == '__main__':
 
-# Add a cool "little big detail"
-message = choice([
-    "Add more tests",
-    "Fixes #9001",
-    "Fixes infinite loop"
-    "Flake8",
-    "Hotfix",
-    "Initial commit",
-    "Optimize the Foobar class",
-    "Reduce the potential yak shaving",
-    "Y2K protection",
-])
+    with open(sys.argv[1]) as f:
+        content = f.read()
 
-if not parse_time_entry(content):
-    print("Please include the amount of time "
-          "spent on this commit in the commit message.")
-    print("E.g. \":clock1: 5m | {}\"".format(message))
-    print("Failed to commit.")
-    sys.exit(1)
+    # Add a cool "little big detail"
+    message = choice([
+        "Add more tests",
+        "Fixes #9001",
+        "Fixes infinite loop"
+        "Flake8",
+        "Hotfix",
+        "Initial commit",
+        "Optimize the Foobar class",
+        "Reduce the potential yak shaving",
+        "Y2K protection",
+    ])
+
+    if not parse_time_entry(content):
+        print("Please include the amount of time "
+              "spent on this commit in the commit message.")
+        print("E.g. \":clock1: 5m | {}\"".format(message))
+        print("Failed to commit.")
+        sys.exit(1)
