852f683f9da0b51efd45d4016c4e0ea3ed635cda
Filip Figiel <filip.figiel@dcs.pl>
2015-11-08 02:44:57 +0000

    :clock1: 30m | Improve the commit-msg hook

--- hooks/install_hooks.bash
+++ hooks/install_hooks.bash
@@ -3,5 +3,5 @@
 CURRDIR=${0%/*}
 BASEDIR=$(git rev-parse --show-toplevel)
 
-ln -s -f ../../hooks/commit-msg.bash ${BASEDIR}/.git/hooks/commit-msg
+ln -s -f ../../hooks/commit-msg.py ${BASEDIR}/.git/hooks/commit-msg
 echo "Hooks installed"
