ea7e9f83143ec2c3f73d9c3f49141f69bdc66892
Filip Figiel <filip.figiel@dcs.pl>
2015-11-08 03:01:27 +0000

    :clock1: 2m | Execute flake8 on pre-commit

--- hooks/install_hooks.bash
+++ hooks/install_hooks.bash
@@ -4,4 +4,5 @@ CURRDIR=${0%/*}
 BASEDIR=$(git rev-parse --show-toplevel)
 
 ln -s -f ../../hooks/commit-msg.py ${BASEDIR}/.git/hooks/commit-msg
+ln -s -f ../../hooks/pre-commit.sh ${BASEDIR}/.git/hooks/pre-commit
 echo "Hooks installed"
