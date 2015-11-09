1e4f355b5173b96ce44785700d7a3585f23fe119
Marek Bednarski <marekb@marekb.waw.pl>
2015-11-09 23:30:12 +0100

    :clock1: 15m | #48 Add tests

--- tests/frontends/cli.py
+++ tests/frontends/cli.py
@@ -1,9 +1,12 @@
+import click
 import pytest
+
 from click.testing import CliRunner
 
 from tests.core.mock_source import MockSource
 
 from time_tracker.frontends.cli.tracker import print_time_tracking_info
+from time_tracker.frontends.cli.options import DependentOption
 
 
 @pytest.fixture(autouse=True)
@@ -38,3 +41,26 @@ def test_cli_fail():
         ['--token', 'dummy', '--repo', 'dummy', '--total', '--issue', 'a']
     )
     assert result.exception
+
+
+# Options tests
+
+def test_dependent_option_prompt():
+    @click.command()
+    @click.option('--option', cls=DependentOption, prompt=True,
+                  prompt_depends_on=('another', False))
+    def test_comm(option):
+        pass
+
+    runner = CliRunner()
+    result = runner.invoke(
+        test_comm,
+        input='a'
+    )
+    assert not result.exception
+
+
+@pytest.mark.parametrize('value', [1, 'a', [], {}])
+def test_dependent_option_create_fail(value):
+    with pytest.raises(TypeError):
+        DependentOption('--option', prompt_depends_on=value)
